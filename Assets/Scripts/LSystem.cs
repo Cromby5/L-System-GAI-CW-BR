30fa99d228002a51d31e52f7706f94d25f540d66
Ben Reid <70808579+Cromby5@users.noreply.github.com>
2022-12-02 01:56:42 +0000

    Leafs, Stochastic rules

--- Assets/Scripts/LSystem.cs
+++ Assets/Scripts/LSystem.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using static UnityEngine.GraphicsBuffer;
-
+using Random = UnityEngine.Random;
 public class LSystem: MonoBehaviour
 {
     [Header("Rules")]
@@ -33,7 +34,7 @@ public class LSystem: MonoBehaviour
     [Header("Objects")]
     [SerializeField] private Material branchMat;
 
-    [SerializeField] private GameObject cube;
+    [SerializeField] private GameObject leaves;
     [SerializeField] private GameObject cylinder;
     
     [Header("Sliders")]
@@ -105,6 +106,17 @@ public class LSystem: MonoBehaviour
             // loop through the current string
             for (int j = 0; j < currentString.Length; j++)
             {
+                float r = Random.Range(0f, 1.0f);
+                float sum = 0.0f;
+                for (int k = 0; k < rules.Count; k++)
+                {
+                    sum += rules[k].probability;
+                    if (r < sum)
+                    {
+                        rule = rules[k].result;
+                        break;
+                    }
+                }
                 // if the current character is an F
                 if (currentString[j] == 'F')
                 {
@@ -148,27 +160,58 @@ public class LSystem: MonoBehaviour
                     // move the position
                     position += rotation * Vector3.up * length;
                     
-                    //meshGenerator.CreateShape(startPosition, position, rotation, width);
+                    meshGenerator.CreateShape(startPosition, position, rotation, width);
                     
                     // move forward draw a line
                     DrawLine(position, rotation, length, width);
                     DrawCylinders(position, rotation, length, width);
+
                     break;
                 case 'f':
                     // move the position forward, without drawing line
                     position += rotation * Vector3.up * length;
                 break;
                 case 'G':
-                    //Nothing
+                    // Nothing - Could be used to generate more in the string building phase with a provided rule 
                     break;
+                // Used to rotate both x and z axis at same time
                 case '+':
                     // rotate the rotation left
-                    rotation *= Quaternion.Euler(-angle, 0f, -angle);
+                    rotation *= Quaternion.Euler(-angle , 0f, -angle);
                 break;
                 case '-':
                     // rotate the rotation right
                     rotation *= Quaternion.Euler(angle, 0f, angle);
-                break;
+                    break;
+                    
+                // X AXIS
+                case 'X':
+                    // rotate the rotation left
+                    rotation *= Quaternion.Euler(-angle, 0f, 0f);
+                    break;
+                case 'x':
+                    // rotate the rotation right
+                    rotation *= Quaternion.Euler(angle, 0f, 0f);
+                    break;
+                // Y AXIS
+                case 'Y':
+                    // rotate the rotation left
+                    rotation *= Quaternion.Euler(0f, -angle, 0f);
+                    break;
+                case 'y':
+                    // rotate the rotation right
+                    rotation *= Quaternion.Euler(0f, angle, 0f);
+                    break;
+                // Z AXIS
+                case 'Z':
+                    // rotate the rotation left
+                    rotation *= Quaternion.Euler(0f, 0f, -angle);
+                    break;
+                case 'z':
+                    // rotate the rotation right
+                    rotation *= Quaternion.Euler(0f, 0f, angle);
+                    break;
+                    
                 case '[':
                     // push the position and rotation to the stack
                     positionStack.Push(new TransformStore { position = position, rotation = rotation, width = width});
@@ -179,7 +222,7 @@ public class LSystem: MonoBehaviour
                     // pop the position and rotation from the stack
                     TransformStore ti = positionStack.Pop();
                     // Place a leaf
-                    children.Add(Instantiate(cube, position, rotation,transform));
+                    children.Add(Instantiate(leaves, position, rotation,transform));
                     // Restore the position and rotation
                     position = ti.position;
                     rotation = ti.rotation;
--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -12,34 +12,39 @@ public class MeshGenerator : MonoBehaviour
     int[] triangles;
     
     public int _basePolygon = 20;
-    
+    public float _radius = 0.1f;
+    public float _height = 1f;
+
     [SerializeField] private GameObject cube;
+    
     // Start is called before the first frame update
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         UpdateMesh();
-
     }
     
     public void CreateShape(Vector3 startPosition, Vector3 position, Quaternion rotation,float width)
     {
         Vector3[] startVertices = new Vector3[_basePolygon];
         Vector2[] startUv = new Vector2[_basePolygon];
-
-        float angularStep = 2f * Mathf.PI / (float)(_basePolygon);
         
-
+        float angularStep = 2f * Mathf.PI / (float)(_basePolygon);
+        float angle = angularStep * _radius;
+        
         for (int j = 0; j < _basePolygon; j++)
         {
-            Vector3 pos = new Vector3(Mathf.Cos(j * angularStep), 0f, Mathf.Sin(j * angularStep)) + position;
-            pos *= 0.6f;
-            GameObject cubes = Instantiate(cube, pos, Quaternion.identity, transform);
+            angle += angularStep;
+
+            Vector3 pos = new Vector3(Mathf.Cos(angle), position.y, Mathf.Sin(angle));
+            Debug.Log(pos);
+            GameObject cubes = Instantiate(cube, _radius * pos, rotation, transform);
+            
             startVertices[j] = pos;
             startUv[j] = new Vector2(j*angularStep, startVertices[j].y);
         }
-        
+        //UpdateMesh();
     }
     void UpdateMesh()
     {
